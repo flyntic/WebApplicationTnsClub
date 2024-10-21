e8eb696a08b5677b84246a1242ff3192329fc009
flyntic <64376713+flyntic@users.noreply.github.com>
2024-10-21 21:40:53 +0300

    Отказ от startup.cs

--- WebApplicationTnsClub/Program.cs
+++ WebApplicationTnsClub/Program.cs
@@ -2,117 +2,69 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using WebApplicationTnsClub;
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        BuildWebHost(args).Run();
-    }
-    public static IWebHost BuildWebHost(string[] args) => WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build();
-}
+using WebApplicationTnsClub.Models;
 
-/*
-using Microsoft.AspNetCore;
-
-
-internal class Program
-{
-
-    public static void Main(string[] args)
-    {
-        BuildWebHost(args).Run();
-    }
-    public static IWebHost BuildWebHost(string[] args) => WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build();
-*/
-    /*  private static void Main(string[] args)
-      {
-          var builder = WebApplication.CreateBuilder(args);
-          builder.Services.AddSpaStaticFiles(configuration =>
-          {
-              configuration.RootPath = "Client.App/dist";
-          });
-
-          var app = builder.Build();
-
-          IConfiguration configuration = app.Configuration;
-          IWebHostEnvironment env = app.Environment;
-
-          if (env.IsDevelopment())
-          {
-              app.UseDeveloperExceptionPage();
-          }
-
-          app.UseStaticFiles();
-          if (!env.IsDevelopment())
-          {
-              app.UseSpaStaticFiles();
-          }
-          app.UseSpa(spa =>
-          {
-              spa.Options.SourcePath = "Client.App";
-
-              if (env.IsDevelopment())
-              {
-                  spa.UseAngularCliServer(npmScript: "start");
-              }
-          });
-
-
-
-          app.UseDefaultFiles();
-          app.UseStaticFiles();
-
-          // Configure the HTTP request pipeline.
-          //if (app.Environment.IsDevelopment())
-          //{
-          // app.UseSwagger();
-          //  app.UseSwaggerUI();
-          //}
-
-          //app.UseHttpsRedirection();
-
-          //app.UseAuthorization();
-
-          //app.MapControllers();
-
-          app.MapGet("/index.html");
-          //app.MapFallbackToFile("/index.html");
-
-          app.Run();
-      }
-}*/
-/*
-
- var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+//builder.Services.AddSwaggerGen();
+
+string connectionString = "Host=localhost;Port=5432;Database=tennisclub;Username=postgres;Password='09870'";
+
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(connectionString),
+    optionsLifetime: ServiceLifetime.Singleton);
+
+builder.Services.AddSpaStaticFiles(configuration =>
+{
+    configuration.RootPath = "ClientApp/dist";
+});
 
 var app = builder.Build();
 
 app.UseDefaultFiles();
-app.UseStaticFiles();
 
 // Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+// app.UseSwagger();
+//  app.UseSwaggerUI();
+//}
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
+app.UseStaticFiles();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSpaStaticFiles();
+}
+app.UseRouting();
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "ClientApp";
+
+    if (app.Environment.IsDevelopment())
+    {
+        spa.UseAngularCliServer(npmScript: "start");
+    }
+});
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
-
-app.MapFallbackToFile("/index.html");
-
 app.Run();
 
- */
