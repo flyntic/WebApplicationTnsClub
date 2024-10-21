37144e24b235c6bbfc0867d07ab5236178bdb0a5
flyntic <64376713+flyntic@users.noreply.github.com>
2024-10-21 20:24:08 +0300

    Добалена асинхронность в работе с бд

--- WebApplicationTnsClub/Controllers/UserController.cs
+++ WebApplicationTnsClub/Controllers/UserController.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 using global::WebApplicationTnsClub.Models;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.EntityFrameworkCore;
 using WebApplicationTnsClub.Models;
 
 namespace WebApplicationTnsClub.Controllers
@@ -17,72 +17,72 @@ namespace WebApplicationTnsClub.Controllers
             db = context;
             if (!db.Users.Any())
             {
-              //  db.Users.Add(new User { name = "iPhone X", last_name = "Apple", login = "79900" });
-              //  db.Users.Add(new User { name = "Galaxy S8", last_name = "Samsung", login = "49900 "});
-              //  db.Users.Add(new User { name = "Pixel 2", last_name = "Google", login = "52900" });
-              //  db.SaveChanges();
+                //  db.Users.Add(new User { name = "iPhone X", last_name = "Apple", login = "79900" });
+                //  db.Users.Add(new User { name = "Galaxy S8", last_name = "Samsung", login = "49900 "});
+                //  db.Users.Add(new User { name = "Pixel 2", last_name = "Google", login = "52900" });
+                //  db.SaveChanges();
             }
         }
-         [HttpGet]
-          public IEnumerable<User> Get()
-          {
-              return db.Users.ToList();
-          }
- /*
+        [HttpGet]
         public async Task<IEnumerable<User>> Get()
         {
-            return db.Users
-                //      .Include(x => x.Id)
-                //      .Select(x =>
-                //          new User
-                 //         {
-                 //             Title = x.Title,
-                 //             DateTime = x.WhenCreated,
-                 //             AuthorName = x.Author?.UserName ?? string.Empty
-                 ////         }
-                 //     )
-                      .ToListAsync();
+            return await db.Users.ToListAsync();
         }
-*/
-      [HttpGet("{id}")]
-        public User Get(int id)
+        /*
+               public async Task<IEnumerable<User>> Get()
+               {
+                   return db.Users
+                       //      .Include(x => x.Id)
+                       //      .Select(x =>
+                       //          new User
+                        //         {
+                        //             Title = x.Title,
+                        //             DateTime = x.WhenCreated,
+                        //             AuthorName = x.Author?.UserName ?? string.Empty
+                        ////         }
+                        //     )
+                             .ToListAsync();
+               }
+       */
+        [HttpGet("{id}")]
+        public async Task<User> Get(int id)
         {
-            User user= db.Users.FirstOrDefault(x => x.Id == id);
+            User user = await db.Users.FirstOrDefaultAsync(x => x.Id == id);
             return user;
         }
 
         [HttpPost]
-        public IActionResult Post(User user)
+        public async Task<IActionResult> Post(User user)
         {
             if (ModelState.IsValid)
             {
-                db.Users.Add(user);
-                db.SaveChanges();
+                await db.Users.AddAsync(user);
+                await db.SaveChangesAsync();
                 return Ok(user);
             }
             return BadRequest(ModelState);
         }
 
         [HttpPut]
-        public IActionResult Put(User user)
+        public async Task<IActionResult> Put(User user)
         {
             if (ModelState.IsValid)
             {
                 db.Update(user);
-                db.SaveChanges();
+                await db.SaveChangesAsync();
                 return Ok(user);
             }
             return BadRequest(ModelState);
         }
 
         [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
-            User user = db.Users.FirstOrDefault(x => x.Id == id);
+            User user = await db.Users.FirstOrDefaultAsync(x => x.Id == id);
             if (user != null)
             {
                 db.Users.Remove(user);
-                db.SaveChanges();
+                await db.SaveChangesAsync();
             }
             return Ok(user);
         }
